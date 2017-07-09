6d5b02ab7217c4265bd765964235fda5f9fa57b7
mohammad-ahmadi <mohammad.ahmad1989@yahoo.com>
2017-07-10 01:06:28 +0430

    Initial

---
+++ build.bat
@@ -0,0 +1,24 @@
+@echo off
+set INP="./src"
+set OUT="./dist"
+
+rm -rf %OUT%
+mkdir %OUT%\css %OUT%\js
+cp %INP%/lib %INP%/images %INP%/fonts %OUT%/ -r
+
+printf "" > %INP%/html/links/root.htm
+printf "" > %INP%/html/scripts/root.htm
+rem printf /static/ > %INP%/html/links/root.htm
+rem printf /static/ > %INP%/html/scripts/root.htm
+
+cmd /c htmlbilder %INP%/html/ -o %OUT%/index.html
+cmd /c handlebars %INP%/templates/template/ -f %OUT%/lib/templates.js -e hbs -m -o
+cmd /c handlebars %INP%/templates/partial/ -f %OUT%/lib/partials.js -p -e hbs -m -o
+cmd /c babel %INP%/js/ -d %OUT%/js/ -s
+
+where sass >nul 2>nul
+if %ERRORLEVEL% EQU 0 (
+	cmd /c sass %INP%/sass/style.scss:%OUT%/css/style.css --style expanded --sourcemap=auto
+) else (
+	cmd /c node-sass %INP%/sass/style.scss > %OUT%/css/style.css --output-style expanded --source-map true
+)
