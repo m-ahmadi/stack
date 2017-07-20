d91270a6eed428540c4fdb85b08f02a01d5250b7
mohammad-ahmadi <mohammad.ahmad1989@yahoo.com>
2017-07-20 10:06:51 +0430

    builds

--- buildr.bat
+++ buildr.bat
@@ -1,9 +1,11 @@
-@echo off
+REM @echo off
 path=%PATH%;./node_modules/.bin/
 set INP=./src
-set OUT=./dist
-set MKO=dist
-set ROOT=
+set OUT=./release/static
+set MKO=release\static
+set HOUT=./release/
+set ROOT=/static/
+set FL=app.js
 
 cmd /c rm -rf %OUT%
 cmd /c mkdir %MKO%\css %MKO%\js
@@ -12,13 +14,19 @@ cmd /c cp %INP%/lib %INP%/images %INP%/fonts %OUT%/ -r
 cmd /c printf %ROOT% > %INP%/html/links/root.htm
 cmd /c printf %ROOT% > %INP%/html/scripts/root.htm
 cmd /c printf %ROOT% > %INP%/html/scripts/app/root.htm
-cmd /c printf "main.js" > %INP%/html/scripts/app/filename.htm
+cmd /c printf %FL% > %INP%/html/scripts/app/filename.htm
 cmd /c printf "define(() => '%ROOT%');" > %INP%/js/core/root.js
 
-cmd /c htmlbilder %INP%/html/ -o %OUT%/index.html
+cmd /c htmlbilder %INP%/html/ -o %HOUT%/index.html
 cmd /c handlebars %INP%/templates/template/ -f %OUT%/lib/templates.js -e hbs -m -o
 cmd /c handlebars %INP%/templates/partial/ -f %OUT%/lib/partials.js -p -e hbs -m -o
-cmd /c babel %INP%/js/ -d %OUT%/js/ -s
+REM cmd /c babel %INP%/js/ -d %OUT%/js/ -s
+
+set TMP=%OUT%/js/app.babel.js
+cmd /c r_js -o baseUrl=%INP%/js/ name=main out=%TMP% optimize=none
+cmd /c babel %TMP% -o %OUT%/js/%FL% -s
+cmd /c rm -f %TMP%
+cmd /c cp %INP%/js/workers/ %OUT%/js/ -r
 
 cmd /c where sass >nul 2>nul
 if %ERRORLEVEL% EQU 0 (
--- buildr.sh
+++ buildr.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 PATH=$PATH:./node_modules/.bin
 INP=./src
-OUT=./dist
+OUT=./release/static
+HOUT=./release
+ROOT=/static/
+FL=app.js
 
 rm -rf $OUT
 mkdir $OUT/css $OUT/js -p
 cp $INP/lib $INP/images $INP/fonts $OUT/ -r
 
-printf "" > $INP/html/links/root.htm
-printf "" > $INP/html/scripts/root.htm
-# printf /static/ > $INP/html/links/root.htm
-# printf /static/ > $INP/html/scripts/root.htm
+printf $ROOT > $INP/html/links/root.htm
+printf $ROOT > $INP/html/scripts/root.htm
+printf $ROOT > $INP/html/scripts/app/root.htm
+printf $FL > $INP/html/scripts/app/filename.htm
+printf "define(() => '$ROOT');" > $INP/js/core/root.js
 
-htmlbilder $INP/html/ -o $OUT/index.html
+htmlbilder $INP/html/ -o $HOUT/index.html
 handlebars $INP/templates/template/ -f $OUT/lib/templates.js -e hbs -m -o
 handlebars $INP/templates/partial/ -f $OUT/lib/partials.js -p -e hbs -m -o
-babel $INP/js/ -d $OUT/js/ -s
+# babel $INP/js/ -d $OUT/js/ -s
+
+TMP=$OUT/js/app.babel.js
+r_js -o baseUrl=$INP/js/ name=main out=$TMP optimize=none
+babel $TMP -o $OUT/js/$FL -s
+rm -f $TMP
+cp $INP/js/workers/ $OUT/js/ -r
 
 if [ -x "$(command -v sass)" ]; then
 	sass $INP/sass/style.scss:$OUT/css/style.css --style expanded --sourcemap=auto
